30887c50f070bca1282a617992fbdb5c6e6621be
deven <devjob.2012@gmail.com>
2020-02-03 21:55:38 +0000

    new File

--- Java11/src/main/java/co/uk/java11/Java11Application.java
+++ Java11/src/main/java/co/uk/java11/Java11Application.java
@@ -1,9 +1,36 @@
 package co.uk.java11;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Java11Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("----LOCAL PARAMETER TYPE INFERENCE FOR LAMBDA EXPRESSIONS---------");
+        System.out.println("----USAGE OF STRING:: REPEAT METHOD TO COPY A STRING---------");
+        repeatStrings();
+    }
+
+    private static void repeatStrings() {
+        String str = "abc\t\n";
+        System.out.println(str.repeat(3));
+        var text = "Hello Java 10";
+        System.out.println(text);
+        var myList = new ArrayList<Map<String, List<Integer>>>();
+
+        for (var current : myList) {
+            // current is infered to type: Map<String, List<Integer>>
+            System.out.println(current);
+        }
+
+        var list = List.of("A", "B", "C");
+        var copy = List.copyOf(list);
+        System.out.println(list == copy);   // true
+
+        var map = Map.of("A", 1, "B", 2);
+        System.out.println(map);    // {B=2, A=1}
     }
 
 }
