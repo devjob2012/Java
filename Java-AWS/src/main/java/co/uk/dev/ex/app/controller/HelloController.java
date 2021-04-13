8856617e0ddf465e1353ab94d59b7119abcf47a5
Deven <Devan.rawat1@nhs.net>
2021-04-13 12:41:28 +0100

    new File

---
+++ Java-AWS/src/main/java/co/uk/dev/ex/app/controller/HelloController.java
@@ -0,0 +1,22 @@
+package co.uk.dev.ex.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        String message = "Hello AWS Continuous Delivery!";
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message += " From host: " + ip;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+}
